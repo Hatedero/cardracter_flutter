3e9f9ecce3ae7d3a036cce2e3879e47b503df229
LucasDevan <lucas.devanlay@etu.univ-lyon1.fr>
2026-01-16 11:46:09 +0100

    Added newCard view

---
+++ lib/newCard/view/newCardView.dart
@@ -0,0 +1,41 @@
+import 'package:cardracter_flutter/app/widgets/CardPreview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart' hide Card;
+import '../../app/widgets/AppBottomBar.dart';
+import '../../app/domain/Card.dart';
+
+class NewCardView extends StatefulWidget {
+  const NewCardView({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<NewCardView> createState() => _NewCardViewState();
+}
+
+class _NewCardViewState extends State<NewCardView> {
+  List<Card> cards = List.empty();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      bottomNavigationBar: AppBottomBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton.icon(
+                onPressed: () => Navigator.of(context).pushNamed("/cardDetails"),
+                icon: Icon(Icons.info),
+                label: const Text("go to detail"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
