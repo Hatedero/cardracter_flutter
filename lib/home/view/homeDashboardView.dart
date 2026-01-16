55f139592ac6a9b4fe4554478d6f1c3f5fda0172
Hatedero <54317277+Hatedero@users.noreply.github.com>
2026-01-16 09:20:17 +0100

    blank state, 3 folders for main pages and functions

---
+++ lib/home/view/homeDashboardView.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class homeDashboardView extends StatefulWidget {
+  const homeDashboardView({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<homeDashboardView> createState() => _homeDashboardViewState();
+}
+
+class _homeDashboardViewState extends State<homeDashboardView> {
+  List<Card> cards = List.empty();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              'counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
