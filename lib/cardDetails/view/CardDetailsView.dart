45792bc38e5a3ebcf5360bfb99c9a6de620c8d56
LucasDevan <lucas.devanlay@etu.univ-lyon1.fr>
2026-01-16 11:20:47 +0100

    Added card details view

---
+++ lib/cardDetails/view/CardDetailsView.dart
@@ -0,0 +1,40 @@
+import 'package:cardracter_flutter/app/widgets/CardPreview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart' hide Card;
+
+import '../../app/domain/Card.dart';
+
+class CardDetailsView extends StatefulWidget {
+  const CardDetailsView({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<CardDetailsView> createState() => _HomeDashboardViewState();
+}
+
+class _HomeDashboardViewState extends State<CardDetailsView> {
+  List<Card> cards = List.empty();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton.icon(
+                onPressed: Navigator.of(context).pop,
+                icon: Icon(Icons.home),
+                label: const Text("go to home page"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
